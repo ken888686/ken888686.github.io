03df6ef22a9369002482f2b8a480ea697cca3837
Aaron <ken888686@gmail.com>
2022-11-21 00:24:32 +0900

    create simple Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
+
 import './App.scss';
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button
-        type="submit"
-        className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:py-3 sm:px-8"
-      >
-        Test
-      </button>
-    </>
+    <Header />
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-sky-500">
+      <div className="container mx-auto py-4 text-white">Header</div>
+    </header>
+  );
+}
+
+export default Header;
