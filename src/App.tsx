9c5e8e492bc2ba70df8ad664e588abaa176518e7
Aaron <ken888686@gmail.com>
2022-11-21 00:16:59 +0900

    create tailwindCSS format

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return <div>{1 + 1}</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button
+        type="submit"
+        className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:py-3 sm:px-8"
+      >
+        Test
+      </button>
+    </>
+  );
 }
 
 export default App;
