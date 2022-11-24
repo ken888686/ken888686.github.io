a3071b30a4e85166c726efb43d2b7020856dc6c9
Aaron <ken888686@gmail.com>
2022-11-24 22:13:26 +0900

    add Provider to provide redux store for app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import reducer from './reducers';
 import './index.scss';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
