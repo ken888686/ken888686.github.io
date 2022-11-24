579ddf7ceab43154f4e3e2a796945dce1cda63da
Aaron <ken888686@gmail.com>
2022-11-24 22:13:38 +0900

    create SongList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from './components/Header';
-
-import './App.scss';
+import SongList from './components/SongList';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <SongList />
+    </div>
   );
 }
 
---
+++ src/components/SongList.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+  render(): React.ReactNode {
+    return <div>Song List</div>;
+  }
+}
+
+export default SongList;
