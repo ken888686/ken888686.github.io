2ad79fad95f68ee7250f31857d94f1a07c955900
Aaron <ken888686@gmail.com>
2022-11-26 10:17:08 +0900

    create eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-createRoot(  document.getElementById('root') as HTMLElement
-).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
