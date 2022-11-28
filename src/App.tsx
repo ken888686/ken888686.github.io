b8e8d2233c7c6364c7cb80703c702897a5a610bd
Aaron <ken888686@gmail.com>
2022-11-28 19:53:14 +0900

    update folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,8 @@
 import React from 'react';
-import Button from './Button';
-import { BsGoogle, BsFacebook, BsInstagram, BsLine, BsLinkedin } from 'react-icons/bs';
+import ButtonPage from './pages/ButtonPage';
 
 function App() {
-  return (
-    <>
-      <div>
-        <Button primary onClick={() => console.log('Google')}>
-          <BsGoogle />
-          Primary
-        </Button>
-        <Button secondary onMouseLeave={() => console.log('Mouse')}>
-          <BsFacebook />
-          Secondary
-        </Button>
-        <Button success>
-          <BsInstagram />
-          Success
-        </Button>
-        <Button warning>
-          <BsLine />
-          Warning
-        </Button>
-        <Button danger>
-          <BsLinkedin />
-          Danger
-        </Button>
-      </div>
-      <div>
-        <Button primary outline>
-          Primary
-        </Button>
-        <Button secondary outline>
-          Secondary
-        </Button>
-        <Button success outline>
-          Success
-        </Button>
-        <Button warning outline>
-          Warning
-        </Button>
-        <Button danger outline>
-          Danger
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Primary
-        </Button>
-        <Button secondary rounded>
-          Secondary
-        </Button>
-        <Button success rounded>
-          Success
-        </Button>
-        <Button warning rounded>
-          Warning
-        </Button>
-        <Button danger rounded>
-          Danger
-        </Button>
-      </div>
-    </>
-  );
+  return <ButtonPage />;
 }
 
 export default App;
---
+++ src/pages/ButtonPage.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Button from '../components/Button';
+import { BsGoogle, BsFacebook, BsInstagram, BsLine, BsLinkedin } from 'react-icons/bs';
+
+function ButtonPage() {
+  return (
+    <>
+      <div>
+        <Button primary onClick={() => console.log('Google')}>
+          <BsGoogle />
+          Primary
+        </Button>
+        <Button secondary onMouseLeave={() => console.log('Mouse')}>
+          <BsFacebook />
+          Secondary
+        </Button>
+        <Button success>
+          <BsInstagram />
+          Success
+        </Button>
+        <Button warning>
+          <BsLine />
+          Warning
+        </Button>
+        <Button danger>
+          <BsLinkedin />
+          Danger
+        </Button>
+      </div>
+      <div>
+        <Button primary outline>
+          Primary
+        </Button>
+        <Button secondary outline>
+          Secondary
+        </Button>
+        <Button success outline>
+          Success
+        </Button>
+        <Button warning outline>
+          Warning
+        </Button>
+        <Button danger outline>
+          Danger
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Primary
+        </Button>
+        <Button secondary rounded>
+          Secondary
+        </Button>
+        <Button success rounded>
+          Success
+        </Button>
+        <Button warning rounded>
+          Warning
+        </Button>
+        <Button danger rounded>
+          Danger
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default ButtonPage;
