d8b1893a089136c1b4aec129feb047eb2f00a56d
Aaron <ken888686@gmail.com>
2022-12-17 12:15:55 +0900

    fix style self-closing

--- src/components/Common/Separator.tsx
+++ src/components/Common/Separator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Separator() {
-  return <div className='my-3 h-[1px] w-full -bg--jet'></div>;
+  return <div className='my-3 h-[1px] w-full -bg--jet' />;
 }
 
 export default Separator;
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -3,7 +3,7 @@ function ErrorPage() {
     <div className='flex h-screen flex-col items-center justify-center -bg--white-1'>
       <h1 className='py-5 text-3xl font-semibold'>Oops!</h1>
       <p className='py-5'>Sorry, an unexpected error has occurred.</p>
-      <p className='italic text-gray-800'></p>
+      <p className='italic text-gray-800' />
     </div>
   );
 }
