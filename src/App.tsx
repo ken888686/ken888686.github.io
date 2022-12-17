ec74e9b01f60245cd9139b3025fc089bc6ffcdc4
Aaron <ken888686@gmail.com>
2022-12-17 10:12:08 +0900

    categorize components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import About from './pages/About';
-import Sidebar from './components/Sidebar';
-import Resume from './pages/Resume';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <main className='container mx-auto px-2 pt-3'>
       <Sidebar />
-      <div className='flex flex-col gap-5'>
-        <About />
-        <Resume />
-      </div>
     </main>
   );
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useTransition } from 'react';
-import { IoChevronDown } from 'react-icons/io5';
+import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
 import ContactList from './ContactList';
 import SocialList from './SocialList';
-import Separator from './Separator';
+import Separator from '../Common/Separator';
+import Avatar from './Avatar';
+import Tag from '../Common/Tag';
 
 const Sidebar = () => {
   const [isPending, startTransition] = useTransition();
-  const [showMore, setShowMore] = useState(false);
+  const [showMore, setShowMore] = useState(true);
 
   const handleClick = () => {
     startTransition(() => {
@@ -21,32 +23,23 @@ const Sidebar = () => {
       overflow-hidden rounded-2xl border -border--jet -bg--eerie-black-2 p-4 -text--white-2 shadow-1 duration-500 ease-in-out`}
     >
       <div className='relative flex items-center justify-start gap-4'>
-        <figure className='rounded-3xl -bg--gradient-onyx'>
-          <img src='/images/my-avatar.png' alt='Yung Chun Tu' width='80' />
-        </figure>
+        <Avatar imageLink='/images/my-avatar.png' name='Yung Chun Tu' />
         <div className=''>
-          <h1
-            className='name mb-2 text-lg font-medium -tracking-wide -text--white-2'
-            title='Yung Chun Tu'
-          >
+          <h1 className='mb-2 text-lg font-medium -tracking-wide -text--white-2'>
             <span className='-from--test bg-gradient-to-r -to--eerie-black-1'>Yung Chun Tu</span>
           </h1>
           <div className='flex gap-2'>
-            <p className='w-max rounded-lg -bg--onyx px-3 py-2 text-xs font-light -text--white-1'>
-              C# Developer
-            </p>
-            <p className='w-max rounded-lg -bg--onyx px-3 py-2 text-xs font-light -text--white-1'>
-              Backend Engineer
-            </p>
+            <Tag title='C# Developer' />
+            <Tag title='Backend Engineer' />
           </div>
         </div>
         <button
-          className='absolute -top-4 -right-4 z-10 rounded-tr-xl rounded-bl-xl -bg--gradient-onyx p-3 text-lg -text--orange-yellow-crayola shadow-2 transition-all before:absolute before:inset-[1px] before:-z-10 before:rounded-[inherit] before:transition-all hover:-text--eerie-black-1 before:hover:-bg--orange-yellow-crayola'
+          className='absolute -top-4 -right-4 z-10 flex items-center justify-center gap-2 rounded-tr-xl rounded-bl-xl -bg--gradient-onyx p-3 text-lg -text--orange-yellow-crayola shadow-2 transition-all before:absolute before:inset-[1px] before:-z-10 before:rounded-[inherit] before:transition-all hover:-text--eerie-black-1 before:hover:-bg--orange-yellow-crayola'
           onClick={handleClick}
           type='button'
         >
-          <span className='hidden'>Show Contacts</span>
-          <IoChevronDown />
+          <span className='hidden md:inline-block'>Show Contacts</span>
+          {showMore ? <IoChevronUp className='text-xl' /> : <IoChevronDown className='text-xl' />}
         </button>
       </div>
 
